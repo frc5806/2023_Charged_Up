db168b1fb6974b62af817806761a89252cf76b32
Shurlyn <sherlynsfashion@gmail.com>
2023-01-12 15:44:00 -0500

    PID

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -28,6 +28,10 @@ public class DriveTrain extends SubsystemBase {
 
     private final AHRS gyro = new AHRS(SPI.Port.kMXP);
 
+    int P, I, D = 1;
+    int integral, previous_error, setpoint = 0;
+    double rcw;
+
   /** Creates a new ExampleSubsystem. */
     public DriveTrain() {
         rightMotors = new CANSparkMax[] {
@@ -62,6 +66,18 @@ public class DriveTrain extends SubsystemBase {
         for (CANSparkMax motor : rightMotors){
             motor.setInverted(true);
         }
+    }
+
+    public void setSetpoint(int setpoint)
+    {
+        this.setpoint = setpoint;
+    }
+
+    public void PID(){
+        double error = setpoint - gyro.getAngle(); // Error = Target - Actual
+        this.integral += (error*.02); // Integral is increased by the error*time (which is .02 seconds using normal IterativeRobot)
+        double derivative = (error - this.previous_error) / .02;
+        this.rcw = P*error + I*this.integral + D*derivative;
     }
 
 // DriveTrain Commands
